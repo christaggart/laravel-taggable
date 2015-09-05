7644c19888c8f9f37d2d047e529efc980ba1f73e
DraperStudio <drprstd@gmail.com>
2015-09-05 17:12:01 +0000

    Add missing copyright

--- src/Traits/Taggable.php
+++ src/Traits/Taggable.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Laravel Database.
+ *
+ * (c) DraperStudio <hello@draperstud.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace DraperStudio\Taggable\Traits;
 
 use DraperStudio\Taggable\Exceptions\InvalidTagException;
